4bfd919569d443f69425d8a3367318c1bfbf7abc
Xavier Thomas-Evelyn <XThomasEvelyn@lcs.on.ca>
2026-06-01 11:27:18 -0400

    Initial Commit

---
+++ ICS3UCulminating/ICS3UCulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICS3UCulminatingApp.swift
+//  ICS3UCulminating
+//
+//  Created by Zave T on 2026-06-01.
+//
+
+import SwiftUI
+
+@main
+struct ICS3UCulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HousesListView()
+        }
+    }
+}
